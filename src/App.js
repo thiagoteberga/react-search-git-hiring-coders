4b39a8d6cd68b62db8b1b2ceb0475c304739816b
thiagoteberga <thiagoteberga@live.com>
2021-07-12 18:34:59 -0300

    Commit Inicial - Iniciando o Curso

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Gama Academy! <code>My Name is Thiago!</code>.
         </p>
         <a
           className="App-link"
